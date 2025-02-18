8a82c79ff71c61c7460a977e5f2dcc19f065db95
ZentCashDevelopers <69402035+ZentCashDevelopers@users.noreply.github.com>
2025-02-18 19:45:55 +0100

    Update win-install.bat

--- scripts/win-install.bat
+++ scripts/win-install.bat
@@ -1,5 +1,5 @@
 set PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.2\bin;%PATH%
-set PATH=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin;%PATH%
+set PATH=C:\Program Files\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin;%PATH%
 set PATH=C:\Program Files\cmake\bin;%PATH%
 
 set CudaToolkitDir=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.2
@@ -7,7 +7,7 @@ set CudaToolkitDir=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.2
 mkdir build
 cd build
 
-cmake -G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_VS_PLATFORM_TOOLSET_CUDA=10.2 .. || type CMakeFiles\CMakeError.log || type CMakeFiles\CMakeOutput.log || exit /b 1
+cmake -G "Visual Studio 17 2022" -A x64 -DCMAKE_BUILD_TYPE=Release -DCMAKE_VS_PLATFORM_TOOLSET_CUDA=10.2 .. || type CMakeFiles\CMakeError.log || type CMakeFiles\CMakeOutput.log || exit /b 1
 
 MSBuild zentcashminer.sln /p:Configuration=Release /m /v:n
 
